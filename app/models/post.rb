a32d8ff5aaa03c134703de55efa89c381f968979
Kazuma <kazuma@example.com>
2019-07-14 18:13:46 +0900

    ページネーションと検索機能の完成。

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,5 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 50 }
   validates :body,  presence: true, length: { maximum: 2000 }
+  scope :find_newest_article, -> { order(created_at: :desc).limit(5) }
 end
